2fe754e4617ad63bb59e8c36426783d12e178c2c
akshayapalanisamy25 <akshayapalanisamy2526@gmail.com>
2026-07-24 19:48:34 +0530

    Added java practice files

---
+++ ifelse2.java
@@ -0,0 +1,14 @@
+import java.util.Scanner;
+
+public class ifelse2 {
+    public static void main(String[] args) {
+        Scanner scan=new Scanner(System.in);
+        String RCB=scan.nextLine();
+        if (RCB.equals("win")){
+            System.out.print("rcb win");
+        }else{
+            System.out.println("lose");
+         }
+        }
+    
+}
