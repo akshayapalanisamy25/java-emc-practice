50d358a8aed6a807275f95ce3c44facab1911f72
akshayapalanisamy25 <akshayapalanisamy2526@gmail.com>
2026-07-27 09:35:55 +0530

    commit

---
+++ array.java
@@ -0,0 +1,9 @@
+public class array {
+    public static void main(String[] args) {
+        int[] num={1,2,3,4,5,6,7,8,9,0};
+        for(int i=0;i<10;i=i+1){
+            System.out.println(num[i]);
+        }
+    }
+    
+}
---
+++ nestedfor.java
@@ -0,0 +1,10 @@
+public class nestedfor {
+    public static void main(String[] args) {
+        for(int i=1;i<=5;i=i+1){
+            for(int j=1;j<=2;j=j+1){
+                System.out.println("hi");
+            }
+            System.out.println("hello");
+        }
+    }
+}
